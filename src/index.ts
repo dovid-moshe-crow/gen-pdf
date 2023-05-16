6d53880cf2c957554aca8ffaea58f85b59a16c3d
dovid-moshe-crow <dovid-moshe-crow@users.noreply.github.com>
2023-05-16 23:55:46 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,58 @@
 import express, { Request, Response } from 'express';
 import PDFDocument from 'pdfkit';
-import fs from 'fs';
 
 const app = express();
+app.use(express.json());
 
-app.get('/generate-pdf', (req: Request, res: Response) => {
-  const text = req.query.text as string; // Assuming the string is passed as a query parameter 'text'
+app.post('/generate-pdf', (req: Request, res: Response) => {
+  const { data } = req.body;
 
   // Create a new PDF document
   const doc = new PDFDocument();
 
-  // Set the response headers
+  // Set the response headers for PDF
   res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'attachment; filename=generated.pdf');
-
-  // Register a Hebrew font
-  doc.registerFont('HebrewFont', './fonts/Rubik-Regular.ttf'); // Replace 'path/to/hebrew-font.ttf' with the actual path to your Hebrew font file
-
-  // Set the font to the Hebrew font
-  doc.font('HebrewFont');
+  res.setHeader('Content-Disposition', 'attachment; filename=table.pdf');
 
   // Pipe the PDF document to the response
   doc.pipe(res);
 
-  // Write the Hebrew text to the PDF
-  doc.text(text, 50, 50); // You can adjust the position (50, 50) according to your needs
+  // Retrieve the keys from the first object to use as table headers
+  const keys = Object.keys(data[0]);
 
-  // End the PDF document
+  const tableWidth = keys.length * 100;
+  const startX = (doc.page.width - tableWidth) / 2;
+
+  // Create table headers
+  doc.font('./fonts/Rubik-Bold.ttf').fontSize(12);
+  let x = startX;
+  let y = 50;
+
+  keys.forEach((key) => {
+    doc.text(key, x, y);
+    x += 100;
+  });
+
+  // Create table rows with data
+  doc.font('./fonts/Rubik-Regular.ttf').fontSize(10);
+  y = 70;
+
+  data.forEach((item: Record<string, any>) => {
+    x = startX;
+
+    keys.forEach((key) => {
+      doc.text(item[key].toString(), x, y);
+      x += 100;
+    });
+
+    y += 20;
+  });
+
+  // Finalize the PDF and end the response
   doc.end();
 });
 
+// Start the server
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log('Server started on port 3000');
 });
